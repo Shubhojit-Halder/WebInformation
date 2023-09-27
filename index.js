74622eed8bb72bfd7248cb51fbd1fd4e97e36d01
Shubhojit Halder <shubhojit_h.ece2020@msit.edu.in>
2023-09-27 21:49:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const IP = require('ip');
+const axios = require('axios');
+const API_KEY = "322b9b2204db4b25b3901d4a37669f34";
+const URL = 'https://ipgeolocation.abstractapi.com/v1/?api_key=' + API_KEY;
+
+const sendAPIRequest = async (ipAddress) => {
+    const apiResponse = await axios.get(URL);
+    return apiResponse.data;
+}
+
+app.get('/', async (req, res) => {
+    const ipAddress = IP.address();
+    const ipAddressInformation = await sendAPIRequest(ipAddress);
+    res.send(ipAddressInformation)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
