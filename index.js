20c509969c945f2870722456f50b9bc434debb0a
Shubhojit Halder <shubhojit_h.ece2020@msit.edu.in>
2023-09-27 22:21:03 +0530

    changes

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const IP = require('ip');
-const axios = require('axios');
+const express = require("express");
+const app = express();
+const port = 3000;
+const IP = require("ip");
+const axios = require("axios");
 const API_KEY = "322b9b2204db4b25b3901d4a37669f34";
-const URL = 'https://ipgeolocation.abstractapi.com/v1/?api_key=' + API_KEY;
+const URL = "https://ipgeolocation.abstractapi.com/v1/?api_key=" + API_KEY;
 
 const sendAPIRequest = async (ipAddress) => {
-    const apiResponse = await axios.get(URL);
+  try {
+    const apiResponse = await axios.get(URL + "&ip_address=" + ipAddress);
     return apiResponse.data;
-}
+  } catch (error) {
+    return -1;
+  }
+};
 
-app.get('/', async (req, res) => {
-    const ipAddress = IP.address();
-    const ipAddressInformation = await sendAPIRequest(ipAddress);
-    res.send(ipAddressInformation)
-})
+app.get("/", async (req, res) => {
+  // const ipAddress = IP.address();
+  try {
+    const ip =
+      req.headers["cf-connecting-ip"] ||
+      req.headers["x-real-ip"] ||
+      req.headers["x-forwarded-for"] ||
+      req.socket.remoteAddress ||
+      "";
+
+    const ipAddressInformation = await sendAPIRequest(ip);
+    res.send(ipAddressInformation);
+  } catch (err) {
+    res.send(err);
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
